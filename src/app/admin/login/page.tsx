4e0ceb6d81f7cdb083c796e71cdd81c0702d2fa9
xTremeLektro <sergio.agustin.cabrera@gmail.com>
2025-09-03 21:10:10 -0400

    removed signup

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -72,12 +72,6 @@ export default function AdminLoginPage() {
             >
               Sign In
             </button>
-            <a
-              href="/admin/signup"
-              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Sign Up
-            </a>
           </div>
         </form>
       </div>
